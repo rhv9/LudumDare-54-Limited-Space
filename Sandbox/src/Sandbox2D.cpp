d647ac8b4a8c5537a595bc41374d3b851f04f617
Gamester1128 <77338063+Gamester1128@users.noreply.github.com>
2023-09-13 00:14:32 +0100

    Added basic profiler test that uses RAII.

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -5,6 +5,51 @@
 #include <imgui/imgui.h>
 #include "Platform/OpenGL/OpenGLShader.h"
 
+#include <chrono>
+
+struct ProfileResult
+{
+	const char* Name;
+	float Time;
+};
+
+static std::vector<ProfileResult> s_ProfileResults;
+
+template <typename Fn>
+class Timer
+{
+public:
+	Timer(const char* name, Fn&& func)
+		: m_Name(name), m_Func(func)
+	{
+		m_StartTimepoint = std::chrono::high_resolution_clock::now();
+	}
+
+	~Timer()
+	{
+		if (!m_Stopped)
+			Stop();
+	}
+
+	void Stop() 
+	{
+		auto endTimepoint = std::chrono::high_resolution_clock::now();
+		m_Stopped = true;
+
+		long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+		long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+		float duration = (float)(end - start) / 1000.0f;
+
+		m_Func({ m_Name, duration });
+	}
+
+private:
+	const char* m_Name;
+	std::chrono::time_point<std::chrono::steady_clock> m_StartTimepoint;
+	bool m_Stopped = false;
+	Fn m_Func;
+};
 
 void Sandbox2DLayer::OnAttach()
 {
@@ -17,6 +62,8 @@ void Sandbox2DLayer::OnDetach()
 
 void Sandbox2DLayer::OnUpdate(Gonk::Timestep ts)
 {
+	Timer timer("Sandbox2D::OnUpdate", [&](ProfileResult profileResult) { s_ProfileResults.push_back(profileResult); });
+
 	m_CameraController.OnUpdate(ts);
 
 	Gonk::RendererCommand::SetColour(glm::vec4{0.2f});
@@ -41,5 +88,16 @@ void Sandbox2DLayer::OnImGuiRender()
 {
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Col", &m_Col[0]);
+
+	for (auto& result : s_ProfileResults)
+	{
+		char label[50];
+		strcpy(label, result.Name);
+		strcat(label, " %.3fms");
+		ImGui::Text(label, result.Time);
+	}
+
+	s_ProfileResults.clear();
+
 	ImGui::End();
 }
